e59350719af33f4289b642169244355ed1da19fb
Yuki Mizuno <superyuki2@berkeley.edu>
2016-12-15 02:03:39 -0800

    Installed first 6 pokemon party

--- main.py
+++ main.py
@@ -2,15 +2,24 @@ import sys
 import pokeAndmoves
 import pokemon
 
+class Player: 
+	def __init__(self, pokemonSet):
+		self.pokemonSet = pokemonSet
+		self.spikes = False #Spikes
+		self.poisonSpikes = False #Poison Spikes
+		self.reflect = False #Reflect
+		self.screen = False #light Screen
+
+
 #Global methods for actual gameplay
 
 def main():
 	print("Welcome to Pokemon Battle Simulator! Your Pokemon will be chosen now.")
-	print([poke.name for poke in pokeAndmoves.allPokemon])
-	print([move.name for move in poke.moves for poke in pokeAndmoves.allPokemon])
-	player = []
-	opponent = []
-	player.append(pokeAndmoves.allPokemon[0])
+	player = Player(pokeAndmoves.allPokemon[0:6])
+	for poke in player.pokemonSet:
+		print(poke.name)
+		for move in poke.moves:
+			print(move.name)
 	print(player)
 
 if __name__ == "__main__":
--- pokemon.py
+++ pokemon.py
@@ -1,22 +1,32 @@
 #The Pokemon Class
 class Pokemon:
-	def __init__(self, name, attribute1, attribute2, status, hp, attack, \
+	def __init__(self, name, attribute1, attribute2, hp, attack, \
 		defense, spAttack, spDefense, speed, moves):
 		#Strings
 		self.name = name
 		self.attribute1 = attribute1
 		self.attribute2 = attribute2
-		self.status = status
-		#original stats, ints
+		self.status = None
+		#base stats, ints
+		self.basehp = hp
+		self.baseattack = attack
+		self.basedefense = defense
+		self.basespAttack = spAttack
+		self.basespDefense = spDefense
+		self.basespeed = speed
+		#modified stats. used in actual battles
 		self.hp = hp
 		self.attack = attack
 		self.defense = defense
 		self.spAttack = spAttack
 		self.spDefense = spDefense
 		self.speed = speed
-		#set of moves, length 4 Move array
-		self.moves = moves
+
+		self.moves = moves #set of moves, length 4 Move array
 		#self.items = items mmmm maybe later
+		#Booleans
+		self.sub = False #Substitute existence
+		self.protect = False #Protect
 
 #The Move Class
 class Move:
@@ -30,4 +40,4 @@ class Move:
 		self.pp = pp
 		#Boolean
 		self.effect = effect
-		self.phys = phys #physical attack - True, special attack - False
+		self.phys = phys #physical attack - True, special attack - False, neither - None
